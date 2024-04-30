590ab6aa0e87948a071e51ed0480c9d0890d46ea
yaroslav <Yaroslav>
2024-04-30 16:54:48 +0300

    Fix collect_data test

--- tests/test_collect_data.py
+++ tests/test_collect_data.py
@@ -1,11 +1,17 @@
 """Tests for collect_data.py."""
+from unittest.mock import patch
+
 import pytest
 
-from backend.collect_data import collect_data, get_loss_and_latency, get_accessibility
+from backend.collect_data import collect_data, get_loss_and_latency, get_accessibility, PORTS
 
 
-def test_collect_data() -> None:
+@patch('backend.collect_data.get_loss_and_latency')
+@patch('backend.collect_data.get_accessibility')
+def test_collect_data(mock_get_accessibility, mock_get_loss_and_latency) -> None:
     """Do call of stable service to test."""
+    mock_get_loss_and_latency.return_value = (0.0, 0.0)
+    mock_get_accessibility.side_effect = lambda host, port: "open" if port in (PORTS["HTTP"], PORTS["HTTPS"]) else "filtered"
     host = "ya.ru"  # Assume super stable
     expected_result = {
         "loss": 0.0,
